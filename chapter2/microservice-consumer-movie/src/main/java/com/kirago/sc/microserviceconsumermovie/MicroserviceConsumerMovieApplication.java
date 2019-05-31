afdefd702e89f8751b9e20be49a536f8533ca958
Tony.zhang <zhangl910201@163.com>
2019-05-31 14:10:29 +0800

    添加spring cloud 示例，根据chapter1改造成springcloud 服务

---
+++ chapter2/microservice-consumer-movie/src/main/java/com/kirago/sc/microserviceconsumermovie/MicroserviceConsumerMovieApplication.java
@@ -0,0 +1,13 @@
+package com.kirago.sc.microserviceconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConsumerMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
+    }
+
+}
---
+++ chapter2/microservice-consumer-movie/src/main/java/com/kirago/sc/microserviceconsumermovie/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.kirago.sc.microserviceconsumermovie.controller;
+
+import com.kirago.sc.microserviceconsumermovie.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id){
+        return restTemplate.getForObject("http://localhost:8002/" + id, User.class);
+    }
+}
