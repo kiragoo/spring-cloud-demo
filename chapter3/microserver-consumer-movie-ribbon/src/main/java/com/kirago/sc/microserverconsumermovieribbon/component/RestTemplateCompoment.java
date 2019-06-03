e4346280abda121b96851aa8fa0b3e66bee6028f
Tony.zhang <zhangl910201@163.com>
2019-06-03 08:59:57 +0800

    添加eureka 结合 ribbon架构的示例，其中遇到的问题会遇到本地开启socket代理后导致consumer与provider的交互出现websokcet I/O报错，调试时需要关闭本地socket代理

---
+++ chapter3/microserver-consumer-movie-ribbon/src/main/java/com/kirago/sc/microserverconsumermovieribbon/component/RestTemplateCompoment.java
@@ -0,0 +1,17 @@
+package com.kirago.sc.microserverconsumermovieribbon.component;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RestTemplateCompoment {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
