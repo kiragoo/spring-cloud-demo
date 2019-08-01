b638501c7ec5e844a39729e3890c30e2922cb8cb
Tony.zhang <zhangl910201@163.com>
2019-08-01 09:17:04 +0800

    删除chapter6

--- chapter6/microservice-provider-user-with-auth/src/main/java/com/kirago/sc/microserviceprovideruserwithauth/component/CustomUserDetailsService.java
+++
@@ -1,4 +0,0 @@
-package com.kirago.sc.microserviceprovideruserwithauth.component;
-
-public class CustomUserDetailsService {
-}
--- chapter6/microservice-provider-user-with-auth/src/main/java/com/kirago/sc/microserviceprovideruserwithauth/config/WebSecurityConfig.java
+++
@@ -1,4 +0,0 @@
-package com.kirago.sc.microserviceprovideruserwithauth.config;
-
-public class WebSecurityConfigAdapter {
-}
