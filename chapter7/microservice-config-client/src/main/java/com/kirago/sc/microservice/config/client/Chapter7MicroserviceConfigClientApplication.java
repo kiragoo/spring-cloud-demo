fdb8c71a8aa174d6bb16514210b00279b4fe57ca
Tony.zhang <zhangl910201@163.com>
2019-08-16 09:33:11 +0800

    添加 spring cloud config 简单示例代码

---
+++ chapter7/microservice-config-client/src/main/java/com/kirago/sc/microservice/config/client/Chapter7MicroserviceConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.kirago.sc.microservice.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter7MicroserviceConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter7MicroserviceConfigClientApplication.class, args);
+	}
+
+}
