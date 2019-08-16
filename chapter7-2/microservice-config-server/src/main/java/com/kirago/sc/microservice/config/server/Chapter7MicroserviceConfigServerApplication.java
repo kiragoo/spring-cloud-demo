6c8dd9f6a61fd5f3522e2aa2a72217a4e667b620
Tony.zhang <zhangl910201@163.com>
2019-08-16 09:38:10 +0800

    添加 spring cloud config 手动刷新配置示例代码

---
+++ chapter7-2/microservice-config-server/src/main/java/com/kirago/sc/microservice/config/server/Chapter7MicroserviceConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.kirago.sc.microservice.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Chapter7MicroserviceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter7MicroserviceConfigServerApplication.class, args);
+    }
+
+}
